9caf22b739a3b94e40c148c9d7ff3225e45a6cd3
phoenix <mcronaldp@gmail.com>
2023-12-28 23:02:01 +0100

    database toegevoegd

--- basispyside.py
+++ basispyside.py
@@ -1,3 +1,10 @@
-print('ronald')
-print('Wat nu')
-print('virtualenv')
+from PySide6.QtWidgets import QApplication
+import sys
+from database import Widget
+
+app = QApplication(sys.argv)
+
+Widget = Widget()
+Widget.show()
+
+app.exec()
---
+++ database.py
@@ -0,0 +1,30 @@
+import sqlite3
+from PySide6.QtWidgets import *
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('SQLiteDatabase')
+        self.setGeometry(100,100,500,100)
+
+    def create_connection(self):
+        pass
+
+    def insert_data(self):
+        pass
+
+    def add_data(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def call_data(self):
+        pass
+
+    def update_data(self):
+        pass
+
+    def delete_data(self):
+        pass
+
